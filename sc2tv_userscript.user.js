ad156c8d0f50d4cf482969c75f2b870a2bf39457
Heart <fromhearttosun@gmail.com>
2013-12-03 13:35:25 +0400

    Добавлена первая версия пользовательского скрипта

---
+++ sc2tv_userscript.user.js
@@ -0,0 +1,168 @@
+// ==UserScript==
+// @name        sc2tv.ru userscript
+// @namespace   http://sc2tv.ru/users/Heart
+// @author      Heart
+// @include     http://sc2tv.ru/*
+// @version     0.0.1
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// ==/UserScript==
+
+(function() {
+    var tabPreferences = (function() {
+        var tabPreferences = {};
+        var commit = function() {
+            var result = [];
+            for (var key in tabPreferences)
+                if (tabPreferences.hasOwnProperty(key))
+                    result.push(key + '^' + tabPreferences[key]);
+            var str = result.join('|');
+            GM_setValue('tab_preferences', str);
+        };
+        (function fetch() {
+            var str = GM_getValue('tab_preferences') || '';
+            str.split('|').forEach(function(preference) {
+                var preferenceParts = preference.split('^');
+                if (preferenceParts.length === 2)
+                    tabPreferences[preferenceParts[0]] = preferenceParts[1];
+            });
+        })();
+        return {
+            preferredTab: function() {
+                return tabPreferences[document.URL];
+            },
+            savePreference: function(tab) {
+                tabPreferences[document.URL] = tab;
+                commit();
+            },
+            clearPreference: function() {
+                delete tabPreferences[document.URL];
+                commit();
+            }
+        };
+    })();
+
+    var grabStreamTabText = function(tab) {
+        return /([^(]+)(?:\(\d+\))?/.exec(tab.firstChild.textContent)[1].trim();
+    };
+
+    (function updateStreamTabs(preferredTabText) {
+        var tabs = document.querySelector('#quicktabs-2 ul');
+        if (!tabs)
+            return;
+        if (!tabs.hasChildNodes())
+            return;
+        var preferredTab = tabs.childNodes[0];
+        var activeTab = preferredTab;
+        [].slice.call(tabs.childNodes, 1).forEach(function(tab) {
+            var tabText = grabStreamTabText(tab);
+            if (tabText === preferredTabText)
+                preferredTab = tab;
+            if (tab.classList.contains('active'))
+                activeTab = tab;
+        });
+        if (preferredTab != activeTab) {
+            activeTab.classList.remove('active');
+            preferredTab.classList.add('active');
+        }
+    })(tabPreferences.preferredTab());
+
+    (function prepareCtrlDropdownMenu() {
+        GM_addStyle('                            \
+            #visual {                            \
+                z-index: 2 !important;           \
+            }                                    \
+            #visual-content, #quicktabs-2 ul {   \
+                overflow: visible !important;    \
+            }                                    \
+            #quicktabs-2 ul li:hover .ctrl-dropdown-menu.enabled { \
+                display: block;                  \
+            }                                    \
+            .ctrl-dropdown-menu {                \
+                display: none;                   \
+                position: absolute;              \
+            }                                    \
+            .ctrl-dropdown-menu-item {           \
+                display: list-item !important;   \
+                text-transform: none !important; \
+                float: none !important;          \
+                color: white !important;         \
+                width: auto !important;          \
+                height: auto !important;         \
+                padding: 0px 10px !important;    \
+                cursor: pointer !important;      \
+            }                                    \
+        ');
+
+        var ctrlHeld = false;
+        document.addEventListener('keydown', function(e) {
+            if (e.key === 'Control') {
+                if (!ctrlHeld)
+                    [].forEach.call(document.getElementsByClassName('ctrl-dropdown-menu'), function(menu) {
+                        menu.classList.add('enabled');
+                    });
+                ctrlHeld = true;
+            }
+
+        });
+        document.addEventListener('keyup', function(e) {
+            if (e.key === 'Control') {
+                if (ctrlHeld)
+                    [].forEach.call(document.getElementsByClassName('ctrl-dropdown-menu'), function(menu) {
+                        menu.classList.remove('enabled');
+                    });
+                ctrlHeld = false;
+            }
+        });
+    })();
+
+    (function injectStreamCtrlDropdownMenu(eraseOld) {
+        var createCtrlDropdownMenu = function(items) {
+            var menu = document.createElement('ul');
+            menu.className = 'ctrl-dropdown-menu';
+            items.forEach(function(item) {
+                var menuItem = document.createElement('li');
+                menuItem.className = 'ctrl-dropdown-menu-item';
+                menuItem.appendChild(document.createTextNode(item.text));
+                menuItem.addEventListener('click', item.action, false);
+                menu.appendChild(menuItem);
+            });
+            return menu;
+        };
+        var createCtrlDropdownMenuItem = function(text, action) {
+            return {
+                text: text,
+                action: action
+            };
+        };
+        var createOpenCtrlDropdownMenuItem = function(tabText) {
+            return createCtrlDropdownMenuItem('Открывать по умолчанию', function() {
+                tabPreferences.savePreference(tabText);
+                injectStreamCtrlDropdownMenu(true);
+            });
+        };
+        var createClearCtrlDropdownMenuItem = function() {
+            return createCtrlDropdownMenuItem('Очистить выбор', function() {
+                tabPreferences.clearPreference();
+                injectStreamCtrlDropdownMenu(true);
+            });
+        };
+
+        var preferredTabText = tabPreferences.preferredTab();
+        var tabs = document.querySelector('#quicktabs-2 ul');
+        if (!tabs)
+            return;
+        [].forEach.call(tabs.childNodes, function(tab) {
+            if (eraseOld)
+                if (tab.lastChild.classList.contains('ctrl-dropdown-menu'))
+                    tab.removeChild(tab.lastChild);
+            var tabText = grabStreamTabText(tab);
+            tab.appendChild(createCtrlDropdownMenu([
+                preferredTabText === tabText ?
+                createClearCtrlDropdownMenuItem() :
+                createOpenCtrlDropdownMenuItem(tabText)
+            ]));
+        });
+    })();
+})();
